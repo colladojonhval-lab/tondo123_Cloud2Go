7284013f4517b44bcb1f9bad0d8e2ccb3a2e5afb
colladojonhval-lab <colladojonhval@gmail.com>
2026-04-08 12:45:52 +0800

    Create Airline_BookingManager.h

---
+++ CODE/Airline_BookingManager.h
@@ -0,0 +1,4 @@
+// PURPOSE: Ito ang "Manager Class" (Ang Utak ng System).
+// Siya ang taga-manage ng LISTAHAN ng lahat ng flights (Array).
+// Dito nakalagay ang mga utos gaya ng "Magdagdag ng Flight" (addFlight) 
+// at "Ipakita ang lahat ng Flight" (showFlights).
