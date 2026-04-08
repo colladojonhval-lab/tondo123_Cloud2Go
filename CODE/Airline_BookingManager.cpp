c81aeef8ac351d7223b70845cb605963f475b398
colladojonhval-lab <colladojonhval@gmail.com>
2026-04-08 12:46:21 +0800

    Update Airline_BookingManager.cpp

--- CODE/Airline_BookingManager.cpp
+++ CODE/Airline_BookingManager.cpp
@@ -1,1 +1,4 @@
-//
+// PURPOSE: Ito ang Implementation ng Manager Class.
+// Dito dapat nakasulat ang mahabang code kung paano mag-save o mag-delete.
+// Dahil 1st year basic setup tayo, ang mga utos ay nasa .h file muna 
+// para hindi tayo magkaroon ng "Spaghetti Code" o buhól-buhól na logic.
