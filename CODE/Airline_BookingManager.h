c983fc86e28b7875165b9d0c4efda09f98c5425b
425001113-yumul <425001113@ntc.edu.ph>
2026-04-27 17:53:53 +0800

    Add Airline Booking Manager class and Flight struct

--- CODE/Airline_BookingManager.h
+++ CODE/Airline_BookingManager.h
@@ -2,3 +2,35 @@
 // Siya ang taga-manage ng LISTAHAN ng lahat ng flights (Array).
 // Dito nakalagay ang mga utos gaya ng "Magdagdag ng Flight" (addFlight) 
 // at "Ipakita ang lahat ng Flight" (showFlights).
+#ifndef AIRLINE_BOOKING_MANAGER_H
+#define AIRLINE_BOOKING_MANAGER_H
+
+#include <string>
+using namespace std;
+
+struct Flight {
+    int flightNumber;
+    string origin;
+    string destination;
+    string country;
+    double price;
+    int totalSeats;
+    int reservedSeats;
+    bool seats[6];
+};
+
+class Airline_BookingManager {
+private:
+    Flight flights[5];
+    int flightCount;
+
+public:
+    Airline_BookingManager();
+
+    void addFlight(Flight f);
+    void showFlights();
+    void searchByBudget(double budget);
+    void processBooking();
+};
+
+#endif
