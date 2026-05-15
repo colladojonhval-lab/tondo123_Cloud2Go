4e2544b4255aabf2f7730d66d76d920014d9bfff
colladojonhval-lab <colladojonhval@gmail.com>
2026-05-16 00:40:09 +0800

    Update Airline_BookingManager.h

--- CODE/Airline_BookingManager.h
+++ CODE/Airline_BookingManager.h
@@ -1,45 +1,46 @@
-#ifndef FLIGHT_H
-#define FLIGHT_H
+#ifndef AIRLINEBOOKINGMANAGER_H
+#define AIRLINEBOOKINGMANAGER_H
+#include <vector>
 #include <string>
+#include "Flight.h"
 
-class Flight {
-private:
-    int flightNumber;
-    std::string destination;
+using namespace std;
+
+struct BookingRecord {
+    string pName;
+    int fID;
+    int sNum;
+};
+
+struct Transaction {
+    string passengerName;
+    string flightDetails;
     double price;
-    bool seats[6];
+    int seatNumber;
+};
+
+class AirlineBookingManager {
+private:
+    vector<Flight> flights;
+    vector<BookingRecord> history;
+    vector<Transaction> transactions; // Lalagyan ng resibo
+    string dbFile = "database.txt";
+
+    void loadData();
+    void saveData();
 
 public:
-    Flight() {
-        flightNumber = 0;
-        destination = "";
-        price = 0.0;
-        for (int i = 0; i < 6; i++) seats[i] = false;
-    }
+    AirlineBookingManager();
 
-    Flight(int fn, std::string dest, double p) {
-        flightNumber = fn;
-        destination = dest;
-        price = p;
-        for (int i = 0; i < 6; i++) seats[i] = false;
-    }
+    void showFlights();
+    void processBooking();
+    bool searchByBudget();
+    void updateFlightPrice();
+    void updatePassengerInfo();
+    void cancelBooking();
+    void viewHistory(); 
 
-    int getFlightNumber() { return flightNumber; }
-    std::string getDestination() { return destination; }
-    double getPrice() { return price; }
-    bool isSeatTaken(int s) { return seats[s]; }
-
-    void setPrice(double p) { price = p; }
-    void reserveSeat(int s) { if (s >= 1 && s <= 5) seats[s] = true; }
-    void freeSeat(int s) { if (s >= 1 && s <= 5) seats[s] = false; }
-
-    std::string getSeatData() {
-        std::string sData = "";
-        for (int i = 1; i <= 5; i++) sData += (seats[i] ? "1" : "0");
-        return sData;
-    }
-    void loadSeatData(std::string data) {
-        for (int i = 0; i < 5; i++) seats[i + 1] = (data[i] == '1');
-    }
+    int getValidatedInt(string prompt);
+    double getValidatedDouble(string prompt);
 };
 #endif
